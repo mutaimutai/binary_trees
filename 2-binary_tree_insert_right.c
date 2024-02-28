5890668f881ad90d5217eeb55800f00ddcf40e5b
mutaimutai <mutaicaleb7539@gmail.com>
2024-02-28 12:50:48 +0300

    function that inserts a node as the right-child of another node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,7 +16,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	binary_tree_t *newnode = binary_tree_node(parent, value);
-
 	if (newnode == NULL)
 		return (NULL);
 
@@ -26,5 +25,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right->parent = newnode;
 	}
 	parent->right = newnode;
+
 	return (newnode);
 }
